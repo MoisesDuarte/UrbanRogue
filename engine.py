e966c789fec0db6a3fb2a98cb012e4c5863a6333
MoisesDuarte <moisesduartejunior@gmail.com>
2019-05-10 09:59:01 -0300

    Setup inicial do projeto

---
+++ engine.py
@@ -0,0 +1,27 @@
+import tcod as libtcod
+
+def main():
+    # Dimensões da tela
+    screen_width = 80
+    screen_height = 50
+    
+    # Especificando arquivo de fonte a ser usada e o tipo de arquivo
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    
+    # Iniciando a tela com dimensões da tela, título e um valor boolean false para iniciar minimizado
+    libtcod.console_init_root(screen_width, screen_height, 'Urban Rogue', False)
+    
+    # Loop do jogo
+    while not libtcod.console_is_window_closed():
+        libtcod.console_set_default_foreground(0, libtcod.white) # Definindo a cor do simbolo '@' como branco
+        libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE) # Desenhando no console 0, que estamos usando, '@' nas coordenadas x e y, com background vazio
+        libtcod.console_flush() # Apresenta os elementos da tela
+        
+        # Fechar jogo
+        key = libtcod.console_check_for_keypress() # Guarda o input do teclado em key
+        if key.vk == libtcod.KEY_ESCAPE: # Se key for igual a esc, retorna True, saindo do loop e fechando o jogo
+            return True
+    
+# A função main apenas será executada quando o script for executado com o comando 'python engine.py'
+if __name__ == '__main__':
+    main()
