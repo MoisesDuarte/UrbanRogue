d4a599a824794406c3323e3b9b8ce2a4c07c26cf
MoisesDuarte <moisesduartejunior@gmail.com>
2019-05-14 09:02:17 -0300

    Implementada função para gerar inimigos

--- engine.py
+++ engine.py
@@ -25,6 +25,9 @@ def main():
     fov_light_walls = True
     fov_radius = 10
     
+    # Atributos para geração de inimigos
+    max_monsters_per_room = 3
+    
     # Cores dos tiles
     colors = {
         'dark_wall': libtcod.Color(49, 46, 47),
@@ -34,9 +37,8 @@ def main():
     }
     
     # Posição dos elementos de jogo (função int usada para cast de resultado de divisão para um integer)
-    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white) # Desenha o personagem
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow) # Desenha o npc
-    entities = [npc, player] # Lista com todas as entidades do mapa
+    player = Entity(0, 0, '@', libtcod.white)
+    entities = [player]
     
     # Especificando arquivo de fonte a ser usada e o tipo de arquivo
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -49,7 +51,7 @@ def main():
     
     # Inicialização do mapa
     game_map = GameMap(map_width, map_height) # Definindo o tamanho do mapa
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player) # Gerando o mapa em si
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room) # Gerando o mapa em si
     
     # Inicialização do FOV
     fov_recompute = True # variavel para processamento de fov
